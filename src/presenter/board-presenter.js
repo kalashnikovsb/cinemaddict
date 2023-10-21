74d45fc4d81efe9e7acbec7ba10f62c57e4ed946
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-10-21 22:10:04 +0700

    Только один попап активен

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -57,11 +57,11 @@ export default class BoardPresenter {
 
 
   #renderFilmPopup = (film) => {
-    const comments = [...this.#commentsModel.getComments(film)];
-
     if (this.#filmPopupComponent) {
       this.#removeFilmPopupComponent();
     }
+
+    const comments = [...this.#commentsModel.getComments(film)];
 
     this.#filmPopupComponent = new PopupView(film, comments);
 
